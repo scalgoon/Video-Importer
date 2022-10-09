79f110817557bb32cdd92929bed2646341430fd8
Dez P <63563412+scalgoon@users.noreply.github.com>
2022-10-09 08:18:12 -0500

    Add files via upload

---
+++ functions/workspace-add.js
@@ -0,0 +1,59 @@
+const execSync = require('child_process').execSync;
+
+const fs = require('fs');
+
+const { JsonDB, Config } = require('node-json-db');
+
+const whoami = execSync('whoami', { encoding: 'utf-8' })
+
+const who = whoami.trim()
+
+const db = new JsonDB(new Config(`/home/${who}/Downloads/Videodeck/database`, true, false, '/'));
+
+module.exports = {
+    addWorkspace: async () => {
+      
+        let data = await db.getData("/");
+
+        const appName = data.appName;
+
+        const folderDir = data.folderDir;
+
+        try {
+
+            const name = execSync('zenity --entry --title "Workspace Name" --text "Please enter the name for the workspace."', { encoding: 'utf-8' })
+
+            const workspaceName = name.trim();
+
+            const workspaceVerify = execSync(`zenity --question --title "Add Workspace" --text "<b>${workspaceName}</b>\nIs this the correct name?" --no-wrap --ok-label "Yes" --cancel-label "No"`, { encoding: 'utf-8' })
+
+            let dir = `${folderDir}/${workspaceName}/`;
+
+            if (fs.existsSync(dir)) {
+
+                const dirExists = execSync(`zenity --error --title "Workspace Exists" --text "The workspace you specified already exists." --no-wrap`, { encoding: 'utf-8' })
+
+                return;
+
+            } else {
+
+                const makeDir = execSync(`zenity --password | sudo -S mkdir "${folderDir}/${workspaceName}"`, { encoding: 'utf-8' })
+
+                let FINAL = execSync(`zenity --info --title "Workspace Added" --text "Workspace <b>${workspaceName}</b> has been added to <b>${appName}</b>" --no-wrap`, { encoding: 'utf-8' })
+
+                return;
+
+            }
+
+        } catch (e) {
+
+            if (e.status === 1) {
+                let END = execSync('zenity --error --title "Process Canceled" --text "Workspace addition process canceled" --no-wrap', { encoding: 'utf-8' })
+            }
+
+            return;
+
+        }
+
+    }
+  }
---
+++ functions/workspace-delete.js
@@ -0,0 +1,59 @@
+const execSync = require('child_process').execSync;
+
+const fs = require('fs');
+
+const { JsonDB, Config } = require('node-json-db');
+
+const whoami = execSync('whoami', { encoding: 'utf-8' })
+
+const who = whoami.trim()
+
+const db = new JsonDB(new Config(`/home/${who}/Downloads/Videodeck/database`, true, false, '/'));
+
+module.exports = {
+    deleteWorkspace: async () => {
+
+        let data = await db.getData("/");
+
+        const appName = data.appName;
+
+        const folderDir = data.folderDir;
+
+        try {
+
+            const output = execSync(`for dir in ${folderDir}/*/; do basename "$dir"; done`, { encoding: 'utf-8' });
+
+            let splitChoices = output.split("\n");
+
+            let choicesArray = Array.from(splitChoices);
+
+            let filteredArray = choicesArray.filter(e => e !== "");
+
+            let useChoices = filteredArray.map((x) => `"*" ${x} \ `);
+
+            const choices = execSync(`zenity --list \
+        --radiolist --title="Delete Workspace" \
+        --column="*" --column="Workspace" \
+            ${useChoices}`, { encoding: 'utf-8' })
+
+            const workspaceName = choices.trim();
+
+            const workspaceVerify = execSync(`zenity --question --title "Delete Workspace" --text "<b>${workspaceName}</b>\nIs this the correct workspace?" --no-wrap --ok-label "Yes" --cancel-label "No"`, { encoding: 'utf-8' })
+
+            const deleteDir = execSync(`zenity --password | sudo -S rm -rf "${folderDir}/${workspaceName}"`, { encoding: 'utf-8' })
+
+            let FINAL = execSync(`zenity --info --title "Workspace Deleted" --text "Workspace <b>${workspaceName}</b> has been deleted." --no-wrap`, { encoding: 'utf-8' })
+
+
+        } catch (e) {
+
+            if (e.status === 1) {
+                let END = execSync('zenity --error --title "Process Canceled" --text "Workspace deletion process canceled" --no-wrap', { encoding: 'utf-8' })
+            }
+
+            return;
+
+        }
+
+    }
+}
