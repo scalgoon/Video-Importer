d19ac0b7ebe3df822c24aea527c316229418039c
Dez P <63563412+scalgoon@users.noreply.github.com>
2022-10-08 21:32:04 -0500

    Update install.js

--- functions/install.js
+++ functions/install.js
@@ -12,7 +12,7 @@ const db = new JsonDB(new Config(`/home/${who}/Downloads/Videodeck/database`, tr
 
 module.exports = {
     installService: async () => {
-      
+
         try {
 
             const START = execSync('zenity --text-info --title "Setup" --filename "./README" --checkbox "I understand the terms above."', { encoding: 'utf-8' })
@@ -41,31 +41,66 @@ module.exports = {
 
             const finalAppName = appName.trim();
 
-            await db.push("/", { appName: finalAppName, folderDir: newFolder });
+            const appSettings = execSync(`zenity --list \
+            --title="${finalAppName} Settings" --width=600 --height=600 \
+            --text "Select which options you want to have." \
+            --checklist \
+            --column "*" \
+            --column "Options" \
+            FALSE "Create Desktop Shortcut" \
+            FALSE "Project Information File" \
+            FALSE "Application Menu Shortcut"`, { encoding: 'utf-8' })
 
-            try {
+            const trimAppSettings = appSettings.trim();
 
-                const desktopShortcut = execSync(`zenity --question --title "Setup for ${finalAppName}" --text "Do you want a desktop shortcut for <b>${finalAppName}</b>?" --no-wrap --ok-label "Yes" --cancel-label "No"`, { encoding: 'utf-8' })
+            const splitAppSettings = trimAppSettings.split("|");
 
-                const makeShortcut = execSync(`cd /home/$USER/Desktop && touch "${finalAppName}.desktop"`, { encoding: 'utf-8' })
+            const arrayAppSettings = Array.from(splitAppSettings);
 
-                const getNode = execSync(`which node`, { encoding: 'utf-8' })
+            const getNode = execSync(`which node`, { encoding: 'utf-8' })
 
-                const nodeVersion = getNode.trim();
-                
-                const writeCode = execSync(`echo "[Desktop Entry]\nType=Application\nEncoding=UTF-8\nName=${finalAppName}\nIcon=/home/${who}/Downloads/Videodeck/accessories-camera.svg\nExec=${nodeVersion} /home/${who}/Downloads/Videodeck/service.js" >> "/home/${who}/Desktop/${finalAppName}.desktop"`, { encoding: 'utf-8' })
+            const nodeVersion = getNode.trim();
 
-                let FINAL = execSync(`zenity --info --title "Setup for ${finalAppName}" --text "<b>${finalAppName}</b> is fully set up." --no-wrap`, { encoding: 'utf-8' })
+            if (arrayAppSettings.includes("Create Desktop Shortcut")) {
 
-            } catch (e) {
+                const makeDeskShortcut = execSync(`cd /home/$USER/Desktop && touch "${finalAppName}.desktop"`, { encoding: 'utf-8' })
 
-                if (e.status === 1) {
-                    let FINAL = execSync(`zenity --info --title "Setup for ${finalAppName}" --text "<b>${finalAppName}</b> is fully set up." --no-wrap`, { encoding: 'utf-8' })
-                }
+                const writeDeskCode = execSync(`echo "[Desktop Entry]\nType=Application\nEncoding=UTF-8\nName=${finalAppName}\nIcon=/home/${who}/Downloads/Videodeck/accessories-camera.svg\nExec=${nodeVersion} /home/${who}/Downloads/Videodeck/service.js\nActions=add-workspace;import-media;delete-project;\n\n[Desktop Action add-workspace]\nName=Add Workspace\nExec=${nodeVersion} /home/${who}/Downloads/Videodeck/app/workspace.js\nIcon=/home/${who}/Downloads/Videodeck/accessories-camera.svg\n\n[Desktop Action import-media]\nName=Import Media\nExec=${nodeVersion} /home/${who}/Downloads/Videodeck/app/import.js\nIcon=/home/${who}/Downloads/Videodeck/accessories-camera.svg\n\n[Desktop Action delete-project]\nName=Delete Project\nExec=${nodeVersion} /home/${who}/Downloads/Videodeck/app/delete.js\nIcon=/home/${who}/Downloads/Videodeck/accessories-camera.svg" >> "/home/${who}/Desktop/${finalAppName}.desktop"`, { encoding: 'utf-8' })
+            }
 
-                return;
+            if (arrayAppSettings.includes("Project Information File")) {
+
+                await db.push("/", { appName: finalAppName, folderDir: newFolder, infoFile: "YES" });
+
+            } else {
+
+                await db.push("/", { appName: finalAppName, folderDir: newFolder });
 
             }
+
+            if (arrayAppSettings.includes("Application Menu Shortcut")) {
+
+                const writeAppCode = execSync(`zenity --password | sudo -S echo "[Desktop Entry]\nType=Application\nEncoding=UTF-8\nName=${finalAppName}\nIcon=/home/${who}/Downloads/Videodeck/accessories-camera.svg\nExec=${nodeVersion} /home/${who}/Downloads/Videodeck/service.js\nActions=add-workspace;import-media;delete-project;\n\n[Desktop Action add-workspace]\nName=Add Workspace\nExec=${nodeVersion} /home/${who}/Downloads/Videodeck/app/workspace.js\nIcon=/home/${who}/Downloads/Videodeck/accessories-camera.svg\n\n[Desktop Action import-media]\nName=Import Media\nExec=${nodeVersion} /home/${who}/Downloads/Videodeck/app/import.js\nIcon=/home/${who}/Downloads/Videodeck/accessories-camera.svg\n\n[Desktop Action delete-project]\nName=Delete Project\nExec=${nodeVersion} /home/${who}/Downloads/Videodeck/app/delete.js\nIcon=/home/${who}/Downloads/Videodeck/accessories-camera.svg" >> "/home/${who}/.local/share/applications/${finalAppName}.desktop"`, { encoding: 'utf-8' })
+
+                try {
+
+                    const restartPrompt = execSync(`zenity --question --title "Restart Prompt" --text "In order for the app to show in your Application menu you must restart the session." --no-wrap --ok-label "Restart now" --cancel-label "Restart later"`, { encoding: 'utf-8' })
+
+                    const restarting = execSync(`reboot`, { encoding: 'utf-8' })
+                
+                } catch (e) {
+
+                    if (e.status === 1) {
+                        let FINAL = execSync(`zenity --info --title "Setup for ${finalAppName}" --text "<b>${finalAppName}</b> is fully set up." --no-wrap`, { encoding: 'utf-8' })
+                    }
+
+                    return;
+
+                }
+
+            }
+
+            let FINAL = execSync(`zenity --info --title "Setup for ${finalAppName}" --text "<b>${finalAppName}</b> is fully set up." --no-wrap`, { encoding: 'utf-8' })
 
         } catch (e) {
 
@@ -78,4 +113,4 @@ module.exports = {
         }
 
     }
-  }
+}
